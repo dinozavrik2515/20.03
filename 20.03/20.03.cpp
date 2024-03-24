373ea672ceed4d415a94f82a8824b3e28040570b
dinozavrik2515 <masha290208@gmail.com>
2024-03-24 18:02:45 +0500

    Добавьте файлы проекта.

---
+++ 20.03/20.03.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+
+
+int main()
+{
+
+	for (int i = 9; i > 0; i--)
+	{
+		if (i == 9)
+		{
+			std::cout << "   a  b  c  d  e  f  g  h\n\n";
+		}
+		if (i == 1)
+		{
+			std::cout << "\n   a  b  c  d  e  f  g  h";
+		}
+		else
+		{
+			std::cout << i - 1 << "  " << "a" << i - 1 << " ";
+			std::cout << "b" << i - 1 << " ";
+			std::cout << "c" << i - 1 << " ";
+			std::cout << "d" << i - 1 << " ";
+			std::cout << "e" << i - 1 << " ";
+			std::cout << "f" << i - 1 << " ";
+			std::cout << "g" << i - 1 << " ";
+			std::cout << "h" << i - 1 << "  " << i - 1;
+			std::cout << "\n";
+		}
+
+	}
+
+}
+
+
